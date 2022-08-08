900173f10e9a0f0eb8f3c79036fe52547ade00ae
Perk7 <denisperkov0@gmail.com>
2022-08-09 01:04:56 +0700

    Добавление индикатора разрешенной геолокации со стороны клиента

--- src/App.js
+++ src/App.js
@@ -7,32 +7,35 @@ import DaysBlock from './components/daysBlock/DaysBlock';
 
 import { useEffect, useState } from 'react';
 
-async function get_weather() {
-  let json;
-  await new Promise(resolve => 
-    navigator.geolocation.getCurrentPosition(async obj => {
-      const res = await fetch('http://' + process.env.REACT_APP_SERVER + `:8000?lat=${obj.coords.latitude}&lon=${obj.coords.longitude}`)
-      json = await res.json()
-      resolve()
-    })
-  ).then(() => {})
-  
-  console.log(json)
-  return json
-}
-
 function App() {
   const [weather, setWeather] = useState(false)
+  const [geoAccess, setGeoAccess] = useState(true)
+
+  async function get_weather() {
+    let json;
+    await new Promise(resolve => 
+      navigator.geolocation.getCurrentPosition(async obj => {
+        setGeoAccess(prevState => true)
+        const res = await fetch('https://' + process.env.REACT_APP_SERVER + `:8000?lat=${obj.coords.latitude}&lon=${obj.coords.longitude}`).catch((e) => console.log(e))
+        json = await res.json()
+        resolve()
+      }, async err => {
+        setGeoAccess(prevState => false)
+        await get_weather()
+      })
+    ).then(() => {})
+    return json
+  }
 
   useEffect(() => {
     if (!weather) {
       const recieve = async () => {
         const data = await get_weather()
-        setWeather(JSON.parse(data))
+        setWeather(prevState => JSON.parse(data))
       }
       recieve()
     }
-  }, [weather])
+  })
 
   return (
     weather
@@ -43,8 +46,8 @@ function App() {
       : <div className='spinner-bg'>
             <h2 className='spinner-bg__heading'>MeTemp</h2>
             <PuffLoader color={'#FFFFFF'} size={100} />
+            {!geoAccess && <h3 className='spinner-bg__geo-alert'>Разрешите доступ к геолокации</h3>}
         </div>
-    
    );
 }
 
